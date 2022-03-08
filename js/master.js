c0258314c6803035245943d176ced06674dcb1c1
Mahmoud Easa <mu01011422865@gmail.com>
2022-03-08 18:48:12 +0200

    Add Landing Page

--- js/master.js
+++ js/master.js
@@ -13,12 +13,22 @@ let faGear = document.querySelector(".settings-box .toggle-settings .fa-gear");
 let colorList = document.querySelectorAll(".colors-list li");
 // Select Random Backgrounds Button Element
 let randomBg = document.querySelectorAll(".random-bg button");
+    // Set backgroundInterval Variable To clearInterval
+    let backgroundToggle = true;
+    let backgroundInterval;
 // Select Show Bullets Element
 let showBullets = document.querySelectorAll(".show-bullets button");
 // Select Reset Options Element
 let resetOPt = document.querySelector(".rest button");
+// Select Bullets
+let bulletsUl = document.querySelector(".bullets ul");
+// Select Span Bullets
+let spanBullets = document.querySelectorAll(".bullets ul li a span");
+// Select Li Bullets
+let liBullets = document.querySelectorAll(".bullets ul li a");
 
 
+// localStorage.clear()
 
 
 
@@ -37,6 +47,43 @@ if (localStorage.getItem("colors")){
         }
     }
 }
+// Add Random Backgrounds Active To Local Storage
+if (localStorage.getItem("randomActive")) {
+    for(let item of randomBg) {
+        item.classList.remove("active");
+        if(item.innerHTML === localStorage.getItem("randomActive")){
+            item.classList.add("active");
+        }
+    }
+}
+if(localStorage.getItem("backgroundToggle")){
+    if (localStorage.getItem("backgroundToggle") == "true") {
+        backgroundToggle = true;
+    } else {
+        if(localStorage.getItem("BackgroundIMG")) {
+            landPag.style.backgroundImage = localStorage.getItem("BackgroundIMG");
+        }
+        backgroundToggle = false;
+    }
+}
+// Add Show Bullets Class To Local Storage
+let showLocal = localStorage.getItem("Show-Bullets");
+if(showLocal) {
+    for(let item of showBullets) {
+        if(item.innerHTML === showLocal) {
+            item.classList.add("active");
+        } else {
+            item.classList.remove("active");
+        }
+        if(showLocal == "Yes") {
+            bulletsUl.style.display = "flex";
+        } else {
+            bulletsUl.style.display = "none";
+        }
+    }
+}
+
+
 
 // Start Landing Page
 // Start Settings Box
@@ -75,35 +122,81 @@ if (localStorage.getItem("colors")){
     }
 
     // Random Backgrounds
-    // Set backgroundInterval Variable To clearInterval
-    let backgroundInterval;
-    
     for(let item of randomBg) {
         item.addEventListener("click", (e)=> {
             for(let item of randomBg) {
                 item.classList.remove("active");
             }
-            if (item.innerHTML === "Yes") {
+            if (item.innerHTML == "Yes") {
+                backgroundToggle = true;
                 toggelBg();
             } else {
+                backgroundToggle = false;
                 // landPag.style.backgroundImage = `url(imgs/01.jpg)`;
                 clearInterval(backgroundInterval);
             }
             e.target.classList.add("active");
+            // Add Active To Local Storage
+            localStorage.setItem("randomActive", e.target.innerHTML);
+            localStorage.setItem("backgroundToggle", backgroundToggle);
+            
         });
     }
+
+    // Show Bullets
+    for(let item of showBullets) {
+        item.addEventListener("click", ()=>{
+            for(let item of showBullets) {
+                item.classList.remove("active");
+            }
+            item.classList.add("active");
+            if(item.innerHTML == "Yes"){
+                bulletsUl.style.display = "flex";
+            } else {
+                bulletsUl.style.display = "none";
+            }
+            localStorage.setItem("Show-Bullets", item.innerHTML);
+        });
+    }
+
+    // Reset Options
+    resetOPt.addEventListener("click", ()=>{
+        localStorage.clear();
+        // backgroundToggle = true;
+        // toggelBg();
+        window.location.reload();
+    });
 // End Settings Box
+
+// Start Bullets
+// Mouseover
+for(let item of liBullets) {
+    // Mouseover Show Span Bullets
+    item.addEventListener("mouseover", ()=>{
+        item.firstChild.style.display = "inline-flex"
+    });
+    // Mouseout Do Not Show Span Bullets
+    item.addEventListener("mouseout", ()=>{
+        for(let item of spanBullets) {
+            item.style.display = "none"
+        }
+    });
+}
+// End Bullets
 
 // Background Landing Page
 function toggelBg(){
+    if (backgroundToggle === true) {
         backgroundInterval = setInterval(()=>{
             // Get Random Num
             let num = Math.floor(Math.random() * imgArray.length);
             
             // Change Background Image Url
             // landPag.style.backgroundImage = 'url("imgs/'+ imgArray[num] +'")';
-            landPag.style.backgroundImage = `url(imgs/0${num + 1}.jpg)`;
+            let bgImg = landPag.style.backgroundImage = `url(imgs/0${num + 1}.jpg)`;
+            localStorage.setItem("BackgroundIMG", bgImg);
         }, 2000);
+    }
 }
 toggelBg();
 // End Landing Page
